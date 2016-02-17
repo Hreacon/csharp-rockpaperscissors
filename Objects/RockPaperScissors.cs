1e26b1226bb5a7c953f4dfb9dff36ec62dbe3f3e
Nicholas Jensen and Matt Newell <student@epicodus.com>
2016-02-17 08:38:42 -0800

    Add test for GetGameKeysPlayer1

--- Objects/RockPaperScissors.cs
+++ Objects/RockPaperScissors.cs
@@ -4,8 +4,44 @@ namespace RockPaperScissorsNS.Objects
 {
   public class RockPaperScissors
   {
+    // initial keys
+    private static Dictionary<char, string> _startKeysPlayer1 = new Dictionary<char, string>() {
+      {'q', "Rock"},
+      {'w', "Paper"},
+      {'e', "Scissors"}
+    };
+    private static Dictionary<char, string> _startKeysPlayer2 = new Dictionary<char, string>() {
+      {'p', "Rock"},
+      {'o', "Paper"},
+      {'i', "Scissors"}
+    };
+    private Dictionary<char,string> _keysPlayer1;
+    private Dictionary<char,string> _keysPlayer2;
+    private string _choicePlayer1;
+    private string _choicePlayer2;
+
     public RockPaperScissors()
     {
+      _keysPlayer1 = _startKeysPlayer1;
+      _keysPlayer2 = _startKeysPlayer2;
+    }
+
+    public Dictionary<char,string> GetGameKeysPlayer1()
+    {
+      return _keysPlayer1;
+    }
+    public Dictionary<char,string> GetGameKeysPlayer2()
+    {
+      return _keysPlayer2;
+    }
+
+    public static Dictionary<char,string> GetOriginalGameKeysPlayer1()
+    {
+      return _startKeysPlayer1;
+    }
+    public static Dictionary<char,string> GetOriginalGameKeysPlayer2()
+    {
+      return _startKeysPlayer2;
     }
   } // end class
 } // end namespace
--- Tests/RockPaperScissorsTest.cs
+++ Tests/RockPaperScissorsTest.cs
@@ -1,9 +1,38 @@
 using Xunit;
 using RockPaperScissorsNS.Objects;
+using System.Collections.Generic;
 namespace RockPaperScissorsNS
 {
   public class RockPaperScissorsTest
   {
+    /*
+      RockPaperScissors - Let's play a game of random skill
+      Constructor takes no arguments
+      Object provides game keys, defaults as qwe for player 1 and poi for player 2
+      Players press the provided keys and they get returned to the object and the object gets passed to the View
+      Then the players can hit enter to play again which returns the the main screen
+      the main screen - players hit space and a countdown appears. Press the buttons at the same time to play
+
+      BDD:
+      Tests -
+      Object provides game keys for player 1 and 2
+      object takes players choices
+      object returns players choices in english format
+      object returns who won, or if its a draw
+      object can customize keys to play with
+      settings page lets uesr change keys
+
+    /**/
+    [Fact]
+    public void GetGameKeysPlayerX_ProvidesKeysToPlayWith()
+    {
+      RockPaperScissors rps = new RockPaperScissors();
+      Dictionary<char, string> keys = new Dictionary<char, string>(){};
+      keys.Add('q', "Rock");
+      keys.Add('w', "Paper");
+      keys.Add('e', "Scissors");
+      Assert.Equal(keys, rps.GetGameKeysPlayer1());
+    }
     /* EXAMPLE
     // Have a queen object that knows what coordinants its at
     [Fact]
